0bd6145a9f050b5cf0d40bf94540a778aaa1cdd4
MaaarcosG <33351664+MaaarcosG@users.noreply.github.com>
2018-01-24 08:16:43 -0600

    2.6 Creando Prefab

--- Assets/Scripts/ScrollingBackground.cs
+++ Assets/Scripts/ScrollingBackground.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ScrollingBackground : MonoBehaviour {
+    private float scrollingSpeed;
 
 	// Use this for initialization
 	void Start () {
-		
+        /*Inicializamos el atributo*/
+        scrollingSpeed = 5f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        /*Funcion para que el Blackground se mueva*/
+        transform.Translate(Vector3.left * scrollingSpeed * Time.deltaTime);
+        /*Funcion que sirve para que se repita el blackground*/
+        if (transform.position.x < -20.4f)
+        {
+            transform.position = new Vector3(20.4f, transform.position.y, transform.position.z);
+        }
 	}
 }
